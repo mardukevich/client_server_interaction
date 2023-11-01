9b137f888eda46f16d651a79cf0f6ffee8b5e2f7
Alexey Petrov <apetrov@mail.megaputer.ru>
2023-11-01 21:08:34 +0300

    configure authorization server building

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/autorization_serve.js', // The entry point of your application
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    entry: './src/authorization_server.ts',
+    target: 'node', 
+    output: {
+      filename: 'authorization_server.js', // Output server bundle file
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
