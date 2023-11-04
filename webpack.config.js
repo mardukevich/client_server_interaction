57ed0d2b01d0c78d504bd8772e5e51734d69975f
Alexey Petrov <apetrov@mail.megaputer.ru>
2023-11-04 17:43:53 +0300

    add mode development

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/authorization_server.ts',
-    target: 'node', 
-    output: {
-      filename: 'authorization_server.js', // Output server bundle file
-      path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-  };
+  mode: 'development',
+  entry: './src/authorization_server.ts',
+  target: 'node', 
+  output: {
+    filename: 'authorization_server.js', // Output server bundle file
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
