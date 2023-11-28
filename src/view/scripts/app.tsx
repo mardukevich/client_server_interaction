5cccf286434373b0a8b5bc82f2c9818974c567a6
mardukevich <math1993@mail.ru>
2023-11-28 22:11:24 +0300

    reinstall antd

--- src/view/scripts/app.tsx
+++ src/view/scripts/app.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Form, Input, Button, Typography } from 'antd';
+
+const { Title } = Typography;
 
 export default function App() {
+  const onFinish = (values: any) => {
+    console.log('Received values:', values);
+    // Implement your form submission logic here
+  };
+
   return (
     <>
-      <h1>Sign in</h1>
-      <form action="/login/psassword" method="post">
-        <section>
-          <label for="username">Username</label>
-          <input id="username" name="username" type="text" autocomplete="username" required autofocus>
-        </section>
-        <section>
-          <label for="current-password">Password</label>
-          <input id="current-password" name="password" type="password" autocomplete="current-password" required>
-        </section>
-        <button type="submit">Sign in</button>
-      </form>
+      <Title level={1}>Sign in</Title>
+      <Form name="loginForm" onFinish={onFinish}>
+        <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+          <Input id="username" autoComplete="username" autoFocus />
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+          <Input.Password id="current-password" autoComplete="current-password" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
     </>
-  )
+  );
 }
