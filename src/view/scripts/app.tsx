3dbce78bc78c6f6e1887da4c5e2e90e0744341e5
mardukevich <math1993@mail.ru>
2023-12-06 09:29:50 +0300

    add title to start page

--- src/view/scripts/app.tsx
+++ src/view/scripts/app.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Login } from './login';
 import { Home } from './home';
 import { Path } from '../../path/paths';
 
-export function App() {
+import { Layout, Typography } from 'antd';
 
+const { Header } = Layout;
+const { Title } = Typography;
+
+
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={Path.Home} element={<Home />} />
-        <Route path={Path.Login} element={<Login />} />
-      </Routes>
+      <Layout>
+        <Header>
+          <Title style={{ color: 'white' }}>Welcome</Title>
+        </Header>
+        <Routes>
+          <Route path={Path.Home} element={<Home />} />
+          <Route path={Path.Login} element={<Login />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
-}
+};
 
+export default App;
