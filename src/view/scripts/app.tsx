ac4817547c032e9f4adf205768b4156885b4f4ba
mardukevich <math1993@mail.ru>
2023-12-06 08:13:27 +0300

    remove unused imports

--- src/view/scripts/app.tsx
+++ src/view/scripts/app.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './login';
-import './App.css';
 
 export function App() {
   const [loggedIn, setLoggedIn] = useState(false)
